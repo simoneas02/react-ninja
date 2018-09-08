a5cbe010fec488bec7ced2f111f79e2b26682953
simoneas02 <simoneas03@gmail.com>
2018-09-08 15:40:55 -0300

    configuration initial for to start project and add recursion function

---
+++ es6/utils/recursion.js
@@ -0,0 +1,11 @@
+const sum = ( arr ) => {
+    if ( arr.length === 0 ) {
+      return 0;
+    }
+
+    const [ head, ...tail ] = arr;
+    return head + sum ( tail );
+  }
+  
+  console.log( sum( [ 1, 2, 3, 4 ] ));
+  
