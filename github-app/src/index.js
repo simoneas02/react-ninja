518b109e4086872f11cec072ac9f4912c51c7893
simoneas02 <simoneas03@gmail.com>
2018-09-07 21:34:37 -0300

    start github-app project

---
+++ github-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<h1>Hello GithubApp</h1>, document.getElementById('root'));
+registerServiceWorker();
